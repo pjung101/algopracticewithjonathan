505f5e5511070efe8eeae32a7b112848109e532c
Pete Jung <pjung101@gmail.com>
2019-09-10 15:34:24 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+function binarySearch(arr,x){
+
+}
+// return 1
+print([1,2,3,4,5,6,7],2)
+// return 5
+print([1,2,3,4,5,6,7],6)
+// return 0
+print([1,2,3,4,5,6,7],1)
+// return 6
+print([1,2,3,4,5,6,7],7)
