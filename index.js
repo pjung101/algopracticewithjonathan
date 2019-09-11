47bb26daa1ac156bd2b75e42c08092905be14b21
Jonathan Jen <45050768+beastgun7@users.noreply.github.com>
2019-09-10 17:02:01 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-function binarySearch(arr,x){
-
+function binarySearch(arr, x) {
+  start = 0;
+  end = arr.length - 1;
+  while (start < end) {
+    mid = (start + end) / 2;
+    if (x < arr[mid]) {
+      end = mid - 1;
+    } else if (x > arr[mid]) {
+      start = mid + 1;
+    } else {
+      return arr[mid];
+    }
+  }
+  return arr[start];
 }
 // return 1
-print([1,2,3,4,5,6,7],2)
+console.log(binarySearch([1, 2, 3, 4, 5, 6, 7], 2));
 // return 5
-print([1,2,3,4,5,6,7],6)
+console.log(binarySearch([1, 2, 3, 4, 5, 6, 7], 6));
 // return 0
-print([1,2,3,4,5,6,7],1)
+console.log(binarySearch([1, 2, 3, 4, 5, 6, 7], 1));
 // return 6
-print([1,2,3,4,5,6,7],7)
+console.log(binarySearch([1, 2, 3, 4, 5, 6, 7], 7));
