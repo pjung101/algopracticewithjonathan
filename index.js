0f384ceac67a8b3aaf55e4cfdc787fa00daae5f2
Jonathan Jen <45050768+beastgun7@users.noreply.github.com>
2019-09-12 00:12:40 -0700

    Updated!

--- index.js
+++ index.js
@@ -28,8 +28,14 @@ console.log(binarySearch([1, 2, 3, 4, 5, 6, 7], 7));
 //return -1
 console.log(binarySearch([1, 2, 3, 4, 5, 6, 7], 9));
 
-function isPalindrome(str){
-
+function isPalidrome(x) {
+  arr = [];
+  for (let i = 0; i < x.length; i++) {
+    if (x[i] != x[x.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
 }
 // true
 console.log(isPalindrome('racecar'))
@@ -41,7 +47,21 @@ console.log(isPalindrome('cat'))
 console.log(isPalindrome('apple'))
 
 function fibonacci(n){
-
+count = 2;
+  arr = [0, 1];
+  z = 0;
+  if (n == 0) {
+    return 0;
+  } else if (n == 1) {
+    return 1;
+  }
+  while (n != count - 1) {
+    z = arr[0] + arr[1];
+    arr[0] = arr[1];
+    arr[1] = z;
+    count++;
+  }
+  return z;
 }
 // 5
 console.log(fibonacci(5))
