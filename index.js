1f1d938ab152230b2661165ff7905ca7741eadda
Pete Jung <pjung101@gmail.com>
2019-09-11 18:06:20 -0700

    updated with two more

--- index.js
+++ index.js
@@ -27,3 +27,27 @@ console.log(binarySearch([1, 2, 3, 4, 5, 6, 7], 1));
 console.log(binarySearch([1, 2, 3, 4, 5, 6, 7], 7));
 //return -1
 console.log(binarySearch([1, 2, 3, 4, 5, 6, 7], 9));
+
+function isPalindrome(str){
+
+}
+// true
+console.log(isPalindrome('racecar'))
+// true
+console.log(isPalindrome('bob'))
+// false
+console.log(isPalindrome('cat'))
+// false
+console.log(isPalindrome('apple'))
+
+function fibonacci(n){
+
+}
+// 5
+console.log(fibonacci(5))
+// 2
+console.log(fibonacci(3))
+// 21
+console.log(fibonacci(8))
+// 13
+console.log(fibonacci(7))
